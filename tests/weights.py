1b1061aea34f15f05f617b1ff09f5d8b017e8e93
lucassid <lucassidpacheco@gmail.com>
2018-04-04 13:28:51 -0300

    novbjednvb

--- tests/weights.py
+++ tests/weights.py
@@ -1,3 +1,9 @@
+import matplotlib as mpl
+mpl.use("agg")
+import matplotlib.pyplot as plt
+font = {'size'   : 15}
+mpl.rc('font', **font)
+
 #listas de entradas e saidas esperadas
 x = [0, 1, 2, 3, 4]
 d = [1, 1.75, 2.53, 3.34, 4.2]
@@ -10,13 +16,14 @@ mi = 0.1
 w0_start = 1.5
 w1_start = 0.5
 
-def adjust(x, d, w0_start, w1_start):
+def adjust(x, d, w0_start, w1_start, filename, pltitle):
 	#pesos iniciais do perceptron
 	w0 = [w0_start]
 	w1 = [w1_start]
 	y0 = w1_start * x[0] + w0_start
 	e0 = d[0] - y0
 	e = [e0]
+	y = [y0]
 	n = 0
 	print ("Iteração 0")
 	print ("O erro nesta iteração é de " + str(e0))
@@ -27,8 +34,8 @@ def adjust(x, d, w0_start, w1_start):
 	for n in range(1, len(x) ):
 		w0.append(w0[n-1] + mi * e[n-1])
 		w1.append(w1[n-1] + mi * e[n-1] * x[n-1])
-		y = w1[n] * x[n] + w0[n]
-		e.append(d[n] - y)
+		y.append(w1[n] * x[n] + w0[n])
+		e.append(d[n] - y[n])
 		print ("Iteração " + str(n + 1))
 		print ("O erro nesta iteração é de " + str(e[n]))
 		print ("O peso w1 é: " + str(w1[n]))
@@ -36,10 +43,26 @@ def adjust(x, d, w0_start, w1_start):
 		print ("\n")
 	w0.append(w0[n] + mi * e[n])
 	w1.append(w1[n] + mi * e[n] * x[n])
+
+	plt.plot(x, d, label="dados")
+	plt.plot(x, y, '--', label="predição")
+	plt.legend()
+	plt.xlabel("Entradas")
+	plt.ylabel("Saídas")
+	plt.title(pltitle)
+	plt.savefig(filename)
+	plt.close()
 	return (w0[-1], w1[-1])
+
 print ("---------------PRIMEIRA ÉPOCA---------------")
-w0_start, w1_start = adjust(x, d, w0_start, w1_start)
+w0_start, w1_start = adjust(x, d, w0_start, w1_start, "a.png", "Primeira Época")
 print("----------------SEGUNDA ÉPOCA----------------")
-adjust(x, d, w0_start, w1_start)
+w0_start, w1_start = adjust(x, d, w0_start, w1_start, "b.png", "Segunda Época")
+print("----------------TERCEIRA ÉPOCA----------------")
+w0_start, w1_start = adjust(x, d, w0_start, w1_start, "c.png", "Terceira Época")
+print("----------------QUARTA ÉPOCA----------------")
+w0_start, w1_start = adjust(x, d, w0_start, w1_start, "d.png", "Quarta Época")
+print("----------------QUINTA ÉPOCA----------------")
+w0_start, w1_start = adjust(x, d, w0_start, w1_start, "e.png", "Quinta Época")
 
 
