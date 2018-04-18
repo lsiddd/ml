c85fcf1a4092d6932d835141b9ae7b25090b9989
lucassid <lucassidpacheco@gmail.com>
2018-04-18 09:30:47 -0300

    ncsfjdvbs

--- tests/multilayer.py
+++ tests/multilayer.py
@@ -5,9 +5,14 @@ import numpy as np
 font = {'size'   : 14}
 mpl.rc('font', **font)
 
+verbose = True
+
+#função sigmoide
 def sigmoid(z):
 	return (np.exp(z) - np.exp(-1 * z)) / (np.exp(z) + np.exp(-1 * z))
 
+#Função que recebe o vetor de entradas e uma matriz de pesos
+# e retorna um vetor de saídas em cada neurônio
 def layer(x, u):
 	r1 = []
 	for n in x:
@@ -19,6 +24,11 @@ def layer(x, u):
 
 			s = 0
 			for j in range(len(i)):
+				# if (verbose):
+				# 	if (j < (len(i) - 1)):
+				# 		print (f'X{j}: {n[j]}')
+				# 	else:
+				# 		pass
 				s = s + i[j] * n[j]
 			h1 .append(sigmoid(s))
 		r1.append(h1)
@@ -27,19 +37,14 @@ def layer(x, u):
 def main():
 	#pesos da primeira camada
 	w1 =[[1,   2, 0],
-		[0.5, 1, 0],
-		[1, 2, 3],
-		[0.5, 0.5, 0.1]]
-	#pesos da segunda camada
-	w2 =[[1, 2, 1, 1, 0.5],
-		[2, 1, 1, 1, 0.5]]
+		[0.5, 1, 0]]
 	#pesos da terceira camada
-	w3 =[[1, 2, 0.5],
+	w2 =[[1, 2, 0.5],
 		[2, 1, 0.5]]
 	#listas de entradas e saídas
 	x = [[0, 0], [0, 1], [1, 0], [1, 1]]
 	d = [[0, 0], [1, 0], [1, 0], [0, 1]]
-	print (layer(layer(layer(x, w1),w2), w3))
+	print (layer(layer(x, w1),w2))
 
 if __name__ == "__main__":
 	main()
